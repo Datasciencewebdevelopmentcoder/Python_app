1b6eed38c26c74f92eca66f7f83a4bab2de2d30e
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2025-11-27 22:48:48 +0000

    fixed for real

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little hh humans people! hello by the way!! 3',
+        'message': 'You are doing great, little humans people! hello by the way!! 3',
         'deployed_on': 'kubernetes'
     })
 
