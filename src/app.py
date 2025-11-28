df5c31f58298754700f9805e41677a3a31b4fa9d
Kasey Nelson <123kasey456@gmail.com>
2025-11-27 18:40:47 -0600

    test workflow

--- src/app.py
+++ src/app.py
@@ -15,7 +15,6 @@ def info():
         'message': 'You are doing great, little humans people! hello by the way!! 3',
         'deployed_on': 'kubernetes',
         'app_version': '1.3.0',
-        'environment': 'staging', 
         'additional_info': 'This is some extra info for testing purposes.'
     })
 
