a1fabe58a4d69edd56875d8593839a5df9468b55
Kasey Nelson <123kasey456@gmail.com>
2025-11-27 18:37:56 -0600

    test message

--- src/app.py
+++ src/app.py
@@ -15,7 +15,8 @@ def info():
         'message': 'You are doing great, little humans people! hello by the way!! 3',
         'deployed_on': 'kubernetes',
         'app_version': '1.3.0',
-        'environment': 'staging'
+        'environment': 'staging', 
+        'additional_info': 'This is some extra info for testing purposes.'
     })
 
 @app.route('/api/v1/healthz')
