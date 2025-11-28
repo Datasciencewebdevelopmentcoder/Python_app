fd58b17c3516794c297b70a4ac4bf562368aa48c
Kasey Nelson <123kasey456@gmail.com>
2025-11-28 16:43:09 -0600

    testing update

--- src/app.py
+++ src/app.py
@@ -12,11 +12,8 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little humans people! hello by the way!! 3',
-        'deployed_on': 'kubernetes',
-        'app_version': '1.3.0',
-        'environment': 'staging',
-        'additional_info': 'This is some extra information for testing.'
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
